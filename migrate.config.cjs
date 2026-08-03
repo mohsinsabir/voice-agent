4a2130c199d3c6d55c12a6814dda5b542d651a2d
Mohsin Sabir <sabirmohsin65@gmail.com>
2026-08-04 01:39:01 +0500

    Fix migrate.config.cjs to use CommonJS so CI migrations can load.

--- migrate.config.cjs
+++ migrate.config.cjs
@@ -1,6 +1,6 @@
-import "dotenv/config";
+require("dotenv/config");
 
-export default {
+module.exports = {
   databaseUrl: process.env.DATABASE_URL_MIGRATE ?? process.env.DATABASE_URL,
   dir: "migrations",
   migrationsTable: "pgmigrations",
