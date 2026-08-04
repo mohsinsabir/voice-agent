9f0065e9592432c991ee460fe2e1144befda3dac
Mohsin Sabir <sabirmohsin65@gmail.com>
2026-08-05 02:24:32 +0500

    Allow unused vars prefixed with underscore so tool arg stripping passes lint.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,10 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   ignorePatterns: ["dist/", "node_modules/", "migrations/", "coverage/"],
   rules: {
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
     "@typescript-eslint/explicit-function-return-type": "off",
   },
 };
